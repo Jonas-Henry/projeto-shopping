77c5218aa60eff608981bf630997852e4db925a8
Jonas-Henry <marceloursulino40@gmail.com>
2025-04-16 17:01:54 -0300

    inicio do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {colors: {
+      clifford: '#da373d',
+      amarelo: '#E6c744',
+      cinza: '#c2cbda'
+    }},
+  },
+  plugins: [],
+}
